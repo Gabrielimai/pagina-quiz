08d72bfb29d9f8651b8c9fe1c6de75b837282516
Gabriel Imai <imaig80@gmail.com>
2023-10-30 10:25:23 -0300

    fit: start of creating questions

---
+++ script.js
@@ -0,0 +1,23 @@
+const questions = [
+  {
+    question: "Qual dos console.log() é uma string?",
+    options: [
+      "console.log(123)",
+      "console.log([...])",
+      "console.log({...})",
+      "console.log('joao')",
+    ],
+    answer: "console.log('joao')",
+  },
+  {
+    question: "Qual a diferença entre == e === ?",
+    options: [
+      "(==) compara tipo e valor e (===) compara apenas o tipo",
+      "(===) compara se é um array e (===) compara se é um objeto",
+      "(==) compara o tipo e (===) compara tipo e valor",
+      "Nenhuma das questões acima",
+    ],
+    answer: "(==) compara o tipo e (===) compara tipo e valor",
+  },
+  // Adicione mais questões seguindo o mesmo padrão
+];
